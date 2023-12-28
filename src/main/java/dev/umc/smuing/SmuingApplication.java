ad98b24aaf74d1abc1753b161b21c4450cc9fc23
Jaeyoung Lim <limjyjustin@gmail.com>
2023-12-28 21:16:36 +0900

    Add files via upload

---
+++ src/main/java/dev/umc/smuing/SmuingApplication.java
@@ -0,0 +1,13 @@
+package dev.umc.smuing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmuingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmuingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/umc/smuing/SmuingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.umc.smuing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmuingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
