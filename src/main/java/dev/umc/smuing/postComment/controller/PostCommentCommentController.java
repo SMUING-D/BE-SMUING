851d57c69abf6e7a7ed634775fe72d0a485a987c
jamey <musoyou1085@gmail.com>
2024-03-05 18:49:57 +0900

    :memo: Docs: 스웨거 설정 - 스웨거 설정 - 스웨거 jwt 설정

--- src/main/java/dev/umc/smuing/postComment/controller/PostCommentCommentController.java
+++ src/main/java/dev/umc/smuing/postComment/controller/PostCommentCommentController.java
@@ -5,6 +5,7 @@ import dev.umc.smuing.global.apiPayload.BaseResponse;
 import dev.umc.smuing.global.token.ExtractUserId;
 import dev.umc.smuing.postComment.dto.PostCommentRequestDto;
 import dev.umc.smuing.postComment.service.PostCommentCommentService;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,7 @@ public class PostCommentCommentController {
     private final PostCommentCommentService postCommentCommentService;
 
     @PostMapping("/{commentId}/comments") // userId는 나중에 토큰으로 교체
+    @Parameter(name = "userId", hidden = true)
     public BaseResponse<?> postCommentComment(@RequestBody PostCommentRequestDto.CommentPostDto commentPostDto, @ExtractUserId Long userId, @PathVariable Long commentId) {
         postCommentCommentService.postCommentComment(commentPostDto, userId, commentId);
         return BaseResponse.onSuccess("대댓글 작성에 성공하였습니다.");
